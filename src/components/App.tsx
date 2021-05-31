3adfa4a2be2e27f3fdd2a6733e749a506d65c51f
stephen <steve.wiggins7@gmail.com>
2021-05-30 21:19:25 -0400

    add sass and nav

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
     </div>
   );
 }
