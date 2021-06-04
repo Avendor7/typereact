2bf5828debcbefa06860c76ab1dc64a494e5d828
Stephen <steve.wiggins7@gmail.com>
2021-06-04 16:57:11 -0400

    sidebar nav

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import MyNav from './MyNav';
+import TopNav from './TopNav';
 import Sidebar from './Sidebar';
 
 
 function App() {
   return (
     <div className="App">
-      <MyNav/>
-      <Sidebar/>
+      <TopNav />
+      <div className="container-fluid">
+        <div className="row">
+          <nav id="sidebar" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+          <Sidebar />
+      </nav>
+          <main className="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+            <h1 className="h2">Dashboard</h1>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,57 +3,41 @@ import './../styles/Sidebar.css';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="d-flex col-2 flex-shrink-0 p-3 text-white bg-dark">
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-          <span className="fs-4">Sidebar</span>
-        </a>
-        <hr />
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
-              
-          Home
-        </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              
-          Dashboard
-        </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-            
-          Orders
-        </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-          Products
-        </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              
-          Customers
-        </a>
-          </li>
-        </ul>
-        <hr />
-          <div className="dropdown">
-            <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">MDO</a>
-      <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-        <li><a className="dropdown-item" href="#">New project...</a></li>
-        <li><a className="dropdown-item" href="#">Settings</a></li>
-        <li><a className="dropdown-item" href="#">Profile</a></li>
-        
-        <li><a className="dropdown-item" href="#">Sign out</a></li>
+    <div className="position-sticky pt-md-5">
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            <span className="ml-2">Dashboard</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <span className="ml-2">Orders</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <span className="ml-2">Products</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <span className="ml-2">Customers</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <span className="ml-2">Reports</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <span className="ml-2">Integrations</span>
+          </a>
+        </li>
       </ul>
     </div>
-  </div>
-          </div>
-          
+
 
   );
 }
