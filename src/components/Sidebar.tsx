9fac7ffacfa2bc27a1272c46f725228e8fcb06df
stephen <steve.wiggins7@gmail.com>
2021-07-12 22:00:17 -0400

    attempted logo and reduce sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,17 +4,16 @@ import { Nav, Navbar } from 'react-bootstrap';
 import Calendar from 'react-calendar';
 import './../styles/Calendar.scss';
 import SidebarLink from './SidebarLink';
+import logo from './../DayJourn_cropped.png';
 
 function Sidebar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="flex-column">
-      <Navbar.Brand href="#home">DayJourn</Navbar.Brand>
+      <Navbar.Brand href="#home"><img src={logo} alt="logo"></img></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <div className="col">
           <div className="row">
-            <hr></hr>
-            <h3 className="header">Quick Links</h3>
             <ul>
               <SidebarLink linktext="Two Weeks Ago" />
               <SidebarLink linktext="Last Week" />
@@ -23,15 +22,9 @@ function Sidebar() {
             </ul>
           </div>
           <div className="row">
-            <hr></hr>
-            <h3 className="header">Calendar</h3>
             <Calendar
               calendarType = "US"
             />
-          </div>
-          <div className="row">
-            <hr></hr>
-            <h3 className="header">Tags</h3>
           </div>
         </div>
       </Navbar.Collapse>
