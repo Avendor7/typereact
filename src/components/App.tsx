4f121832163b06b3fbf9199f3c8ac91cb8dedb98
stephen <steve.wiggins7@gmail.com>
2021-07-30 18:39:55 -0400

    pass date to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, FC} from 'react';
+import * as dateFns from 'date-fns';
 import './../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Sidebar from './Sidebar';
@@ -6,13 +7,32 @@ import DayNav from './DayNav';
 import MainWindow from './MainWindow';
 
 
-function App() {
+const App:FC = () =>{
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  let start = dateFns.startOfWeek(selectedDate);
+  let end = dateFns.endOfWeek(selectedDate);
+
+  //loop through dates between start and end of a week
+  const getDaysArray = (start: Date , end: Date) => {
+    for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
+        arr.push(new Date(dt));
+    }
+    return arr;
+  };
+
+  function handleChange(newSelectedDate: Date){
+    setSelectedDate(newSelectedDate);
+    
+  }
+console.log(selectedDate);
   return (
     <div className="App">
       <div className="container-fluid">
         <div className="row">
           <div className="position-fixed bg-dark">
-            <Sidebar />
+            <Sidebar selectedDate={selectedDate} onChange={handleChange}/>
           </div>
           <main className="mainWindow">
             <DayNav />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import * as dateFns from 'date-fns';
+import React, {useState, FC} from 'react';
 import './../styles/Sidebar.scss';
 import { Nav, Navbar } from 'react-bootstrap';
 import Calendar from 'react-calendar';
@@ -8,23 +7,15 @@ import SidebarLink from './SidebarLink';
 import logo from './../DayJourn_cropped.png';
 import UserMenu from './UserMenu';
 
-function Sidebar() {
+interface SidebarProps {
+  selectedDate: Date,
+  onChange(newDate:Date):void
+}
 
-  let [value, onChange] = useState(new Date());
 
-  let start = dateFns.startOfWeek(value);
-  let end = dateFns.endOfWeek(value);
-
-  //loop through dates between start and end of a week
-  const getDaysArray = (start: Date , end: Date) => {
-    for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
-        arr.push(new Date(dt));
-    }
-    return arr;
-  };
-
-  getDaysArray(start,end);
+const Sidebar:FC<SidebarProps> = ({selectedDate, onChange}) =>{
   
+   
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="flex-column sidebar">
       <Navbar.Brand href="#home"><h2>Dayjourn</h2></Navbar.Brand>
@@ -43,7 +34,7 @@ function Sidebar() {
             <Calendar
               calendarType = "US"
               onChange = {onChange}
-              value = {value}
+              value = {selectedDate}
             />
           </div>
           <div className="row">
