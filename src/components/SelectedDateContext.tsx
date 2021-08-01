5c53a0543070199a5932f0e2caf5a6e366be3c9d
Stephen <steve.wiggins7@gmail.com>
2021-08-01 17:14:26 -0400

    add some comments

--- src/components/SelectedDateContext.tsx
+++ src/components/SelectedDateContext.tsx
@@ -4,14 +4,16 @@ interface SelectedDateInterface {
     selectedDate: Date,
     setDate: DateSetter
 }
-
+//define the type for the selectedDate onChange event
 export type DateSetter = (newDate:Date)=>void;
 
+//define the context in accordance to the above interface
 export const SelectedDateContext = createContext<SelectedDateInterface>({
     selectedDate:new Date(),
     setDate:() =>{}
 });
 
+//helper that uses the context so I don't need to call this in every single place I want to use the date
 export const useSelectedDate = ():[Date,DateSetter] =>{
     const {selectedDate,setDate} = useContext(SelectedDateContext);
     return [selectedDate, setDate];
