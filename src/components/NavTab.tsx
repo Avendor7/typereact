3ba7f4af0bc8fe8ed66475d1c2874b7a954b708f
Stephen <steve.wiggins7@gmail.com>
2021-08-02 11:02:27 -0400

    same day function

--- src/components/NavTab.tsx
+++ src/components/NavTab.tsx
@@ -7,21 +7,12 @@ interface NavTabProps{
     number : Date;
     onChange:DateSetter 
 }
-//selectedDate is initialized with the current full date. 
-///Need to strip the time so the dateFns.isEqual function will be true
-function stripTime(date:Date){
-    date = dateFns.setHours(date,0); 
-    date = dateFns.setMilliseconds(date,0);
-    date = dateFns.setMinutes(date,0);
-    date = dateFns.setSeconds(date,0);
-    return(date);
-}
 
 const NavTab: FC<NavTabProps> = ({number, onChange}) => {
     
     const [selectedDate, setDate] = useSelectedDate();
 
-    let buttonClassName = dateFns.isEqual(stripTime(selectedDate),number)  ? "tabButtonActive" : "tabButton";
+    let buttonClassName = dateFns.isSameDay(selectedDate,number)  ? "tabButtonActive" : "tabButton";
 
     return (
         <div className="tab">
