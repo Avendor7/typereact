078d72410c63439067beb47231b4f749c01ba8be
Stephen <steve.wiggins7@gmail.com>
2021-08-02 18:51:37 -0400

    Add the VS Code editor

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './../styles/MainWindow.scss';
+import MonacoEditor from 'react-monaco-editor/lib/editor';
 
 function MainWindow() {
+    
+    const [editorCode, onChange]= useState('');
+    
+    const options = {
+        selectOnLineNumbers: true
+    };
     return (
         <div className="viewbox">
-            <h1>Hello world</h1>
+            <MonacoEditor
+                height="100%"
+                width="100%"
+                language="javascript"
+                theme="vs-dark"
+                value={editorCode}
+                options={options}
+                onChange={onChange}
+            />
         </div>
     );
 }
