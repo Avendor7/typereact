608bda6968c191122298c3aa723658e2c8b0a677
Stephen <steve.wiggins7@gmail.com>
2021-08-28 20:15:38 -0400

    attempt to add tabs

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -2,12 +2,15 @@ import React, {useState} from 'react';
 import './../styles/MainWindow.scss';
 import MonacoEditor from 'react-monaco-editor/lib/editor';
 import BottomStatus from './BottomStatus';
+import {Tabs, Tab} from 'react-bootstrap';
+
 
 function MainWindow() {
     
     const [editorCode, onChange]= useState('');
     
     const options = {
+
         selectOnLineNumbers: true,
         fontSize: 18,
         padding: {
@@ -17,17 +20,32 @@ function MainWindow() {
     };
     return (
         <div className="viewbox">
-            <div className="editor">
-                <MonacoEditor
-                    height="100%"
-                    width="100%"
-                    language="javascript"
-                    theme="vs-dark"
-                    value={editorCode}
-                    options={options}
-                    onChange={onChange}
-                />
+            <div>
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3">
+                    <Tab eventKey="home" title="Home">
+                        <div className="editor">
+                            <MonacoEditor
+                                height="100%"
+                                width="100%"
+                                language="javascript"
+                                theme="vs-dark"
+
+                                value={editorCode}
+                                options={options}
+                                onChange={onChange}
+                            />
+                        </div>
+                    </Tab>
+                    <Tab eventKey="profile" title="Profile">
+                        <h2>Hello World2</h2>
+                    </Tab>
+                    <Tab eventKey="contact" title="Contact" >
+                        <h2>Hello World3</h2>
+                    </Tab>
+                </Tabs>
             </div>
+
+
             <div className="statusbar">
                 <BottomStatus />
             </div>
