97cb02e5fcc820b91c7a9769ba3eb93539340166
Stephen <steve.wiggins7@gmail.com>
2021-09-05 18:50:37 -0400

    material

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,10 @@ import Sidebar from './Sidebar';
 import DayNav from './DayNav';
 import MainWindow from './MainWindow';
 import { SelectedDateProvider } from './SelectedDateContext';
-
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 const App:FC = () => {
 
   const [selectedDate, setSelectedDate] = useState(new Date());
