d6afa2eac76b022d032687d133bf33580af0c358
Stephen <steve.wiggins7@gmail.com>
2021-09-05 17:08:15 -0400

    add material ui

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -2,11 +2,46 @@ import React, {useState} from 'react';
 import './../styles/MainWindow.scss';
 import MonacoEditor from 'react-monaco-editor/lib/editor';
 import BottomStatus from './BottomStatus';
-import {Tabs, Tab} from 'react-bootstrap';
 
+import {Tabs, Tab, Box, Typography} from '@mui/material';
+import {createTheme, ThemeProvider, styled} from '@mui/material/styles';
+import { orange } from '@mui/material/co';
 
-function MainWindow() {
-    
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+  }
+  
+  function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  
+  export default function MainWindow() {
+
     const [editorCode, onChange]= useState('');
     
     const options = {
@@ -18,39 +53,48 @@ function MainWindow() {
             bottom: 20
         }
     };
+
+
+    const [value, setValue] = React.useState(0);
+  
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+  
     return (
-        <div className="viewbox">
-            <div>
-                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3 darktabs">
-                    <Tab eventKey="home" title="Home" className="bg-dark darktab">
-                        <div className="editor">
-                            <MonacoEditor
-                                height="100%"
-                                width="100%"
-                                language="javascript"
-                                theme="vs-dark"
-
-                                value={editorCode}
-                                options={options}
-                                onChange={onChange}
-                            />
-                        </div>
-                    </Tab>
-                    <Tab eventKey="profile" title="Profile">
-                        <h2>Hello World2</h2>
-                    </Tab>
-                    <Tab eventKey="contact" title="Contact" >
-                        <h2>Hello World3</h2>
-                    </Tab>
+    <div className="viewbox">
+        <ThemeProvider theme={dark}>
+             <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Item One" {...a11yProps(0)} />
+                    <Tab label="Item Two" {...a11yProps(1)} />
+                    <Tab label="Item Three" {...a11yProps(2)} />
                 </Tabs>
-            </div>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <div className="editor">
+                        <MonacoEditor
+                            height="100%"
+                            width="100%"
+                            language="javascript"
+                            theme="vs-dark"
 
-
-            <div className="statusbar">
-                <BottomStatus />
-            </div>
-        </div>
+                            value={editorCode}
+                            options={options}
+                            onChange={onChange}
+                        />
+                    </div>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                Item Three
+                </TabPanel>
+            </Box>
+        </ThemeProvider>
+      </div>
     );
-}
+  }
 
-export default MainWindow;
